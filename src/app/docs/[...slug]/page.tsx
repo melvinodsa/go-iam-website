653f4d24fe58257024e72455cfdb0ad8a6c503c3
Melvin Davis <melvinodsa@gmail.com>
2025-11-14 16:33:35 +0530

    on this page margin bottom fix

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -154,7 +154,7 @@ export default async function Docs({
       </Row>
 
 
-      <Column gap="16" maxWidth={layout.sideNav.width} m={{ hide: true }} position="sticky" top="80" overflow="auto" style={{ height: "calc(100vh - var(--static-space-80))" }}>
+      <Column gap="16" maxWidth={layout.sideNav.width} m={{ hide: true }} position="sticky" top="80" overflow="auto" style={{ height: "calc(100vh - var(--static-space-80)*2)" }}>
         <Row gap="12" paddingLeft="2" vertical="center" onBackground="neutral-medium" textVariant="label-default-s">
           <Icon name="book" size="xs" />
           On this page
